3693d281fe18d90e01f6da9d6efd72786a729812
Philip Dimitratos <pdimit@microsoft.com>
2018-02-02 14:33:56 -0800

    WORK IN PROGRESS

--- src/reducers/reducerHelpers/binarySearchReducer/defaultActionDefinitions.js
+++ src/reducers/reducerHelpers/binarySearchReducer/defaultActionDefinitions.js
@@ -1,9 +1,7 @@
 export const defaultActionDefinitions = ({
     insertSingle: 'INSERT_SINGLE',
     insertSingleSelector: action => action.record,
-    insertMultiple: 'INSERT_MULTIPLE',
-    insertMultipleSelector: action => action.records,
-    removeMultiple: 'REMOVE_MULTIPLE'
+    insertMultiple: 'INSERT_MULTIPLE'
 })
 
 export default defaultActionDefinitions
--- src/reducers/reducerHelpers/binarySearchReducer/insertSingle.js
+++ src/reducers/reducerHelpers/binarySearchReducer/insertSingle.js
@@ -1,39 +1,75 @@
 import defaultActionDefinitions from './defaultActionDefinitions'
 import { emptyNode } from './node'
 
+const defaultArrayCapacity = 128
+
 export const insertSingle = (compare, state, record) => {
-    if(!state || !state.pivot){ //Uninitialized child node
-        return {
-            pivot: record,
-            array: [record]
-        }
+  if (!state) { //Uninitialized
+    const newlyCreatedArray = new Array(defaultArrayCapacity)
+    const recordInsertionIndex = Math.floor(defaultArrayCapacity / 2)
+    newlyCreatedArray[recordInsertionIndex] = record
+    return {
+      firstElementIndex: recordInsertionIndex,
+      lastElementIndex: recordInsertionIndex,
+      array: newlyCreatedArray
+    }
+  }
+
+  let floor = state.firstElementIndex
+  let ceiling = state.lastElementIndex
+  let currentPivot = Math.floor((floor + ceiling) / 2)
+
+  while (floor != ceiling) {
+    const comparison = compare(record, state.array[currentPivot])
+    if (!comparison) { //Collision
+      return state
     }
 
-    const comparison = compare(record, state.pivot)
-    if(!comparison){ //Collision
-        return state
+    if (comparison < 0) {
+      ceiling = currentPivot
+    } else { //comparison must be > 0
+      floor = currentPivot
     }
 
-    const operationToPerform = comparison < 0
-        //record less than pivot
-        ? state.left
-            ? state.right
-                ? state.left.array.length > state.right.array.length
-                    //Will inserting left unbalance the tree?
-                    ? pushPivotRight
-                    : insertLeft
-                : pushPivotRight
-            : insertLeft
-        : state.right
-            ? state.left
-                ? state.left.array.length < state.right.array.length
-                    //Will inserting right unbalance the tree?
-                    ? pushPivotLeft
-                    : insertRight
-                : pushPivotLeft
-            : insertRight
+    currentPivot = Math.floor((floor + ceiling) / 2)
+  }
 
-    return operationToPerform(compare)(state, record)
+  const openSpacesAtFrontOfArray = state.firstElementIndex
+  const openSpacesAtEndOfArray = state.array.length - state.lastElementIndex - 1
+  const arrayHasRoom = (openSpacesAtFrontOfArray + openSpacesAtEndOfArray) > 0
+
+  if (arayHasRoom) {
+    const newArray = new Array(state.array.length)
+
+    if (openSpacesAtEndOfArray > openSpacesAtFrontOfArray) {
+      newArray.splice(state.firstElementIndex, currentPivot)
+    }
+  }
+  const newArray = new Array(arrayHasRoom ? state.array.length : (state.array.length * 2))
+
+
+
+
+  const operationToPerform = comparison < 0
+      //record less than pivot
+      ? state.left
+          ? state.right
+              ? state.left.array.length > state.right.array.length
+                  //Will inserting left unbalance the tree?
+                  ? pushPivotRight
+                  : insertLeft
+              : pushPivotRight
+          : insertLeft
+      : state.right
+          ? state.left
+              ? state.left.array.length < state.right.array.length
+                  //Will inserting right unbalance the tree?
+                  ? pushPivotLeft
+                  : insertRight
+              : pushPivotLeft
+          : insertRight
+
+  return operationToPerform(compare)(state, record)
 }
 
 const insertRight = (compare) => (localState, localRecord) => {
